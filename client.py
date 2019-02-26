8c3633007a67ea39411971df4b192dfe4a90d089
ChristosCM <christoumavropoulos.christos@gmail.com>
2019-02-26 17:39:58 +0000

    basic infastructure is set up

--- client.py
+++ client.py
@@ -1,23 +1,27 @@
 from __future__ import print_function
 import sys
+import Pyro4
+import Pyro4.util
 
 if sys.version_info < (3,0):
     input = raw_input
 
 class Client(object):
     def __init__(self,name):
-        self.name = name
-        self.servers = set()
-
+        self._name = name
+        self.main()
     
-    def start(self):
-        source = {
-            server.besID: server.status() for server in self.servers
-        }
-        # while True:
-        #     for market, quote_source in quote_sources.items():
-        #         quote = next(quote_source)  # get a new stock quote from the source
-        #         symbol, value = quote
-        #         if symbol in self.symbols:
-        #             print("{0}.{1}: {2}".format(, symbol, value))
-            
+    def findFront(self):
+        servers = []
+        with Pyro4.locateNS() as ns:
+            for server, server_uri in ns.list(prefix="front.").items():
+                print("found server: ", server)
+                servers.append(Pyro4.Proxy(server_uri))
+        if not servers:
+            raise ValueError("No fornt-end servers were found. This might be an error or all the servers are currently offline.")
+        return servers[0]
+    def main(self):
+        server = self.findFront()
+
+Client("Mikey")
+
--- server.py
+++ server.py
@@ -1,25 +1,41 @@
 from __future__ import print_function
 import Pyro4
 import csv
+import time
+import random
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
 
 class Server(object):
-    def __init__(self,besID):
-        self.movies = "ml-latest-small/movies.csv"
-        self.ratings = "ml-latest-small/ratings.csv"
+    def __init__(self,name,besID,status):
+        self.movieFile = "ml-latest-small/movies.csv"
+        self.ratingsFile = "ml-latest-small/ratings.csv"
         self._besID = besID
-        self._status = set()
+        self._status = status
+        self._name = name
         
-        with open(self.movies) as file:
+        with open(self.movieFile) as file:
             reader = csv.reader(file)
-            self.contents = ["reader","1"]
-        print (self.contents)
+            self.movies = list(reader)
 
+        with open(self.ratingsFile) as file:
+            reader = csv.reader(file)
+            self.ratings = list(reader)
+
+    @property
     def status(self):
-        return self.status
+        return self._status
+    @property
+    def name(self):
+        return self._name
+    @property
+    def identifier(self):
+        return self._identifier
 
+    def setStatus(self,status):
+        self._status = status
+        return "Status Updated"
     def list_contents(self):
         return self.contents
     def take(self,name,item):
@@ -31,9 +47,9 @@ class Server(object):
 
 
 if __name__ == "__main__":
-    server1 = Server(1)
-    server2 = Server(2)
-    server3 = Server(3)
+    server1 = Server("first",1,"ACTIVE")
+    server2 = Server("second",2,"OVER-LOADED")
+    server3 = Server("third",3,"OFFLINE")
     with Pyro4.Daemon() as daemon:
         server1_uri = daemon.register(server1)
         server2_uri = daemon.register(server2)
@@ -43,9 +59,9 @@ if __name__ == "__main__":
             ns.register("bes.server1", server1_uri)
             ns.register("bes.server2", server2_uri)
             ns.register("bes.server3", server3_uri)
-        server1.status = "active"
-        server2.status = "over-loaded"
-        server3.status = "offline"
-        print("Servers available.")
+        # server1.status = "ACTIVE"
+        # server2.status = "OVER-LOADED"
+        # server3.status = "OFFLINE"
+        print("Servers are now available.")
         daemon.requestLoop()
 
